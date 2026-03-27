d869049c029b6b0b807c0f5d7d9f09a097582baf
NeptuUn <82344789+NeptuUn@users.noreply.github.com>
2026-03-27 09:30:57 +0100

    feat(migrations): add database migration scripts for roles, permissions, and user tables

--- src/lib/db/migrations/0002_public_onslaught.sql
+++ src/lib/db/migrations/0002_public_onslaught.sql
@@ -1,3 +1,11 @@
-ALTER TABLE "counters" ADD COLUMN "share_token" text;--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "counter_history_counter_id_changed_at_idx" ON "counter_history" ("counter_id","changed_at");--> statement-breakpoint
-ALTER TABLE "counters" ADD CONSTRAINT "counters_share_token_unique" UNIQUE("share_token");
+ALTER TABLE "counters"
+ADD COLUMN IF NOT EXISTS "share_token" text;
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "counter_history_counter_id_changed_at_idx" ON "counter_history" ("counter_id", "changed_at");
+--> statement-breakpoint
+DO $$ BEGIN
+ALTER TABLE "counters"
+ADD CONSTRAINT "counters_share_token_unique" UNIQUE("share_token");
+EXCEPTION
+WHEN duplicate_object THEN null;
+END $$;
